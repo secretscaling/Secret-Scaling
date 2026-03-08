cd557df02e0f47b6ffb6b5755447d5355d17e8cc
Berkay <secretscaling@gmail.com>
2026-03-08 16:02:50 +0100

    favicon

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/Images/Logo/Favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Open+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
